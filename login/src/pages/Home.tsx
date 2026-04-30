97d2b3bfed6964a9f7e87339adad466597192a45
yedraalumna <yedrasanchezsantana@gmail.com>
2026-04-30 14:04:22 +0100

    arreglada la carpeta login para subir sus archivos

---
+++ login/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+//Componente HOME
+
+import { Typography, Container } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Container sx={{ mt: 5 }}>
+      <Typography variant="h3" color="primary" textAlign="center">
+        Página Home de Yedra
+      </Typography>
+    </Container>
+  );
+}
